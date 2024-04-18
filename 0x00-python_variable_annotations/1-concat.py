719941753b50c9c638dd710f47f7cff6cc5e481a
NeneNafe <nenechiwaya@gmail.com>
2024-04-18 22:23:31 +0200

    a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str) -> str:
+    """A function that concatenates two strings
+
+    Args:
+        str1 (str): a string type
+        str2 (str): string type
+
+    Returns:
+        str: concatenated string of both variables
+    """
+    return str1 + str2
