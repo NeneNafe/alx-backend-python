07ab72d64a18ae7f5ceb352176ac4f8a56a2ca39
NeneNafe <nenechiwaya@gmail.com>
2024-04-22 15:57:22 +0200

    Corrections

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-"""an async routine called wait_n that takes in 2 int arguments (in this order): n and max_delay. You will spawn
-wait_random n times with the specified max_delay.
+"""an async routine called wait_n that takes in 2 int arguments
+You will spawn wait_random n times with the specified max_delay.
 """
 
 
@@ -14,7 +14,7 @@ async def wait_n(n: int, max_delay: int) -> list:
     """a function that will spawn wait_random n times"""
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    
+
     sorted_delays = [delay for delay in sorted(delays)]
-    
+
     return sorted_delays
