a1eb0e2a29f847833f347e44064463a0f3795b67
NeneNafe <nenechiwaya@gmail.com>
2024-04-22 20:28:24 +0200

    A function that uses a coroutine

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""a function task_wait_random that takes an integer max_delay
+and returns a asyncio.Task"""
+
+import asyncio
+from asyncio import create_task
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """A function tht returns a text"""
+    async def coro():
+        await wait_random(max_delay)
+
+    return create_task(coro())
