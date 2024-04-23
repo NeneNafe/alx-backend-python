cbd7b364bd3ad760bae20e1dc50fdbfe02915f4c
NeneNafe <nenechiwaya@gmail.com>
2024-04-23 20:20:20 +0200

    a coroutine called async_generator that takes no arguments

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""a coroutine called async_generator that takes no arguments"""
+
+import random
+import asyncio
+
+
+async def async_generator():
+    """Takes no args but loops 10 times by picking a random number
+    between the numbers 0 - 10"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.randint(0, 10)
---
+++ 0x02-python_async_comprehension/README.md
@@ -0,0 +1,7 @@
+PROJECT TITLE: 0x02-python_async_comprehension
+
+Tasks
+
+0. Async Generator
+1. Async Comprehensions
+2. Run time for four parallel comprehensions
