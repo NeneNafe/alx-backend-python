3900d0a05e26f33b5f4308cf0d83b24e813fbe5c
NeneNafe <nenechiwaya@gmail.com>
2024-04-23 20:26:40 +0200

    Corrections

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -10,4 +10,4 @@ async def async_generator():
     between the numbers 0 - 10"""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.randint(0, 10)
+        yield random.uniform(0, 10)
