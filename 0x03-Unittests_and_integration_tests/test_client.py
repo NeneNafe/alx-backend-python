d34acddb941f4825d04a0f6e01d037dbaf1b0ab6
NeneNafe <nenechiwaya@gmail.com>
2024-05-13 20:41:01 +0200

    Tals 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -33,3 +33,20 @@ class TestGithubOrgClient(TestCase):
         with patch(get_mock, PropertyMock(return_value=payload)):
             client = GithubOrgClient("x")
             self.assertEqual(client._public_repos_url, output)
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ unit-test GithubOrgClient.public_repos """
+        nene = {"name": "Nene", "license": {"key": "a"}}
+        hope = {"name": "Hope", "license": {"key": "b"}}
+        vann = {"name": "Vann"}
+        get_mock = 'client.GithubOrgClient._public_repos_url'
+        mock_get_json.return_value = [nene, hope, vann]
+        with patch(get_mock, PropertyMock(return_value="www.yes.com")) as i:
+            actual = GithubOrgClient("x")
+            self.assertEqual(actual.public_repos(), ['Nene', 'Hope', 'Vann'])
+            self.assertEqual(actual.public_repos("a"), ['Nene'])
+            self.assertEqual(actual.public_repos("c"), [])
+            self.assertEqual(actual.public_repos(45), [])
+            mock_get_json.assert_called_once_with("www.yes.com")
+            i.assert_called_once_with()
